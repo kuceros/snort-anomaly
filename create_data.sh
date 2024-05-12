e00f4d0404fe0b305c356b3f24439c1d2b44317d
SedanJakoLeh <57224322+SedanJakoLeh@users.noreply.github.com>
2024-05-12 19:45:34 +0200

    update data script

--- create_data.sh
+++ create_data.sh
@@ -8,14 +8,22 @@
 # Usage: create_data.sh <path_to_top_folder>
 # Rostislav Kucera <kucera.rosta@gmail.com>, 2024
 
+
 process_files() {
     local folder="$1"
     local snort_command="$2"
+    local total_files=$(find "$folder" -maxdepth 1 -type f | wc -l | awk '{print $1}')
+    local processed_files=0
+
     for file in "$folder"/*; do
         if [[ -f "$file" ]]; then
             execute_snort "$snort_command" "$file"
+            ((processed_files++))
+            printf "\r\033[K"
+            echo -n "$processed_files/$total_files"
         fi
     done
+    printf "\n" 
 }
 
 execute_snort() {
@@ -25,7 +33,7 @@ execute_snort() {
 }
 
 if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <path_to_top_folder>"
+    echo "Usage: $0 <path_to_folder>"
     exit 1
 fi
 
@@ -37,14 +45,12 @@ fi
 snort_command="snort -c \"/usr/local/etc/snort/snort.lua\" --plugin-path \"/usr/local/snort/lib/snort/plugins/extra/\" -q >/dev/null 2>&1"
 
 for subdirectory1 in "$1"/*; do
-    if [[  -d "$subdirectory1" ]]; then
-        if [[ "$(basename "$subdirectory1")" != "captures3" ]]; then
-            for subdirectory2 in "$subdirectory1"/*; do
-                if [[ -d "$subdirectory2" ]]; then
-                    # Process files in each subdirectory
-                    process_files "$subdirectory2" "$snort_command"
-                fi
-            done
-        fi
+    if [[ -d "$subdirectory1" ]]; then
+        for subdirectory2 in "$subdirectory1"/*; do
+            if [[ -d "$subdirectory2" ]]; then
+                echo "Processing files in: $subdirectory2"
+                process_files "$subdirectory2" "$snort_command"
+            fi
+        done
     fi
 done
